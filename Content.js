37a6412e04f1703cbc6f75a3f9a66903404cd986
Ian Alrahwan <ianalrahwan@utexas.edu>
2022-03-05 15:18:26 -0600

    added a button to div containing username

--- Content.js
+++ Content.js
@@ -2,7 +2,11 @@ function hello() {
     const all_users = document.querySelectorAll('.sqdOP.yWX7d._8A5w5.ZIAjV');
 
     for (let user_ of all_users) {
-        user_.innerHTML = "<i>we stored this username</i>"
+        if (user_.querySelector('#added') === null) {
+            var button = document.createElement("BUTTON");
+            button.innerHTML = "<button id = added class='absolute bottom - 0 right - 0 rounded - md shadow m - 2'>&#128065</button>";
+            user_.appendChild(button)
+        }
     };
     // alert(`Stored ${all_users.length} usernames`);
 
