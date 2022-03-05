a0f42044dee223e8d2ee6458194493f85ed9ea00
Ian Alrahwan <ianalrahwan@utexas.edu>
2022-03-05 15:28:09 -0600

    Added Flags and Button support - solve merge conflict

--- Content.js
+++ Content.js
@@ -1,7 +1,62 @@
+var people = ["kimkardashian", "ye", "marcusfilly", "universityhousinganddining"];
+// 1 represents Community Flag, 5 computer flag, "abc": a:Image Edited b: Loaded Language c: Extra
+var flags_list = ["100", "010", "510", "010"];
+
+// CO_ -> implies community flagged, ML_ -> computer flagged
+// ImageEdit: Known to edit their photos
+// Lang: Flagged to use loaded language
+
+
 function hello() {
     const all_users = document.querySelectorAll('.sqdOP.yWX7d._8A5w5.ZIAjV');
 
     for (let user_ of all_users) {
+        let index = people.indexOf(user_.innerHTML);
+        if (index != -1) {
+            let flags = flags_list[index];
+            let added_flags = false;
+            if (flags[0] == '1') {
+                user_.innerHTML = user_.innerHTML + " | " + "CO_ImageEdit";
+                added_flags = true;
+            }
+            if (flags[0] == '5') {
+                user_.innerHTML = user_.innerHTML + " | " + "ML_ImageEdit";
+                added_flags = true;
+            }
+            if (flags[1] == '1') {
+                if (added_flags == true) {
+                    user_.innerHTML = user_.innerHTML + ", " + "CO_Lang";
+                } else {
+                    user_.innerHTML = user_.innerHTML + " | " + "CO_Lang";
+                    added_flags = true;
+                }
+            }
+            if (flags[1] == '5') {
+                if (added_flags == true) {
+                    user_.innerHTML = user_.innerHTML + ", " + "ML_Lang";
+                } else {
+                    user_.innerHTML = user_.innerHTML + ", " + "ML_Lang";
+                    added_flags = true;
+                }
+            }
+
+            if (flags[2] == '1') {
+                if (added_flags == true) {
+                    user_.innerHTML = user_.innerHTML + ", " + "CO_Extra";
+                } else {
+                    user_.innerHTML = user_.innerHTML + " | " + "CO_Extra";
+                    added_flags = true;
+                }
+            }
+            if (flags[2] == '5') {
+                if (added_flags == true) {
+                    user_.innerHTML = user_.innerHTML + ", " + "ML_Extra";
+                } else {
+                    user_.innerHTML = user_.innerHTML + ", " + "ML_Extra";
+                    added_flags = true;
+                }
+            }
+        }
         if (user_.querySelector('#added') === null) {
             var button = document.createElement("BUTTON");
             button.innerHTML = "<button id = added class='absolute bottom - 0 right - 0 rounded - md shadow m - 2'>&#128065</button>";
