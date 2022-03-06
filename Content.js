ac86e296df23b122f99a7a4c9601eca0d8ec2936
Ian Alrahwan <ianalrahwan@utexas.edu>
2022-03-05 20:31:41 -0600

    added color change and removed tag display

--- Content.js
+++ Content.js
@@ -1,63 +1,25 @@
 var people = ["kimkardashian", "ye", "marcusfilly", "universityhousinganddining"];
-// 1 represents Community Flag, 5 computer flag, "abc": a:Image Edited b: Loaded Language c: Extra
-var flags_list = ["100", "010", "510", "010"];
+
+/*
+The first digit indicates whether the user alters their photos.
+The second digit indicates whether the user spreads nutritional misinformation.
+Both follow the convention below:
+0: Not altered
+1: Crowd-source database confirmed
+2: ML suspecting
+*/
+var flags_list = ["10", "01", "21", "01"];
 
 // CO_ -> implies community flagged, ML_ -> computer flagged
 // ImageEdit: Known to edit their photos
-// Lang: Flagged to use loaded language
+// Lang: Nutritional misinfo
 
 
 function hello() {
     const all_users = document.querySelectorAll('.sqdOP.yWX7d._8A5w5.ZIAjV');
 
     for (let user_ of all_users) {
-        let index = people.indexOf(user_.innerHTML);
-        if (index != -1) {
-            let flags = flags_list[index];
-            let added_flags = false;
-            if (flags[0] == '1') {
-                user_.innerHTML = user_.innerHTML + " | " + "CO_ImageEdit";
-                added_flags = true;
-            }
-            if (flags[0] == '5') {
-                user_.innerHTML = user_.innerHTML + " | " + "ML_ImageEdit";
-                added_flags = true;
-            }
-            if (flags[1] == '1') {
-                if (added_flags == true) {
-                    user_.innerHTML = user_.innerHTML + ", " + "CO_Lang";
-                } else {
-                    user_.innerHTML = user_.innerHTML + " | " + "CO_Lang";
-                    added_flags = true;
-                }
-            }
-            if (flags[1] == '5') {
-                if (added_flags == true) {
-                    user_.innerHTML = user_.innerHTML + ", " + "ML_Lang";
-                } else {
-                    user_.innerHTML = user_.innerHTML + ", " + "ML_Lang";
-                    added_flags = true;
-                }
-            }
-
-            if (flags[2] == '1') {
-                if (added_flags == true) {
-                    user_.innerHTML = user_.innerHTML + ", " + "CO_Extra";
-                } else {
-                    user_.innerHTML = user_.innerHTML + " | " + "CO_Extra";
-                    added_flags = true;
-                }
-            }
-            if (flags[2] == '5') {
-                if (added_flags == true) {
-                    user_.innerHTML = user_.innerHTML + ", " + "ML_Extra";
-                } else {
-                    user_.innerHTML = user_.innerHTML + ", " + "ML_Extra";
-                    added_flags = true;
-                }
-            }
-        }
-        if ((user_.querySelector('#added') === null) && (!user_.querySelector('._7UhW9.PIoXz.qyrsm.KV-D4.uL8Hv'))) {
+        if ((user_.parentElement.parentElement.querySelector('#added') === null) && (!user_.querySelector('._7UhW9.PIoXz.qyrsm.KV-D4.uL8Hv'))) {
             var button = document.createElement("BUTTON");
             button.style.backgroundColor = "white";
             button.innerHTML = "&#129528";
@@ -65,7 +27,53 @@ function hello() {
             button.id = "added"
             button.style.borderColor = "grey"
             button.style.borderRadius = "20px"
-            user_.appendChild(button)
+            let index = people.indexOf(user_.innerHTML);
+            if (index != -1) {
+                let flags = flags_list[index];
+                let added_flags = false;
+                if (flags[0] == '1') {
+                    button.style.backgroundColor = "#FF6961";
+                    added_flags = true;
+                }
+                if (flags[0] == '2') {
+                    button.style.backgroundColor = "yellow";
+                    added_flags = true;
+                }
+                if (flags[1] == '1') {
+                    if (added_flags == true) {
+                        user_.innerHTML = user_.innerHTML + ", " + "CO_Lang";
+                    } else {
+                        user_.innerHTML = user_.innerHTML + " | " + "CO_Lang";
+                        added_flags = true;
+                    }
+                }
+                if (flags[1] == '5') {
+                    if (added_flags == true) {
+                        user_.innerHTML = user_.innerHTML + ", " + "ML_Lang";
+                    } else {
+                        user_.innerHTML = user_.innerHTML + ", " + "ML_Lang";
+                        added_flags = true;
+                    }
+                }
+
+                if (flags[2] == '1') {
+                    if (added_flags == true) {
+                        user_.innerHTML = user_.innerHTML + ", " + "CO_Extra";
+                    } else {
+                        user_.innerHTML = user_.innerHTML + " | " + "CO_Extra";
+                        added_flags = true;
+                    }
+                }
+                if (flags[2] == '5') {
+                    if (added_flags == true) {
+                        user_.innerHTML = user_.innerHTML + ", " + "ML_Extra";
+                    } else {
+                        user_.innerHTML = user_.innerHTML + ", " + "ML_Extra";
+                        added_flags = true;
+                    }
+                }
+            }
+            user_.parentElement.parentElement.appendChild(button)
         }
     };
     // alert(`Stored ${all_users.length} usernames`);
