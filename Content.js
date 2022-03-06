8b25c556a87b83f25b2a2ec68ee92417a4b14670
Ian Alrahwan <ianalrahwan@utexas.edu>
2022-03-05 23:29:50 -0600

    added an alert on button push

--- Content.js
+++ Content.js
@@ -8,7 +8,7 @@ Both follow the convention below:
 1: Crowd-source database confirmed
 2: ML suspecting
 */
-var flags_list = ["10", "01", "21", "01"];
+var flags_list = ["11", "01", "21", "01"];
 
 // CO_ -> implies community flagged, ML_ -> computer flagged
 // ImageEdit: Known to edit their photos
@@ -30,33 +30,63 @@ function hello() {
             let index = people.indexOf(user_.innerHTML);
             if (index != -1) {
                 let flags = flags_list[index];
-                let added_flags = false;
                 if (flags[0] == '1') {
                     button.style.backgroundColor = "#FF6961";
-                    added_flags = true;
                 }
                 if (flags[0] == '2') {
                     button.style.backgroundColor = "yellow";
-                    added_flags = true;
                 }
                 if (flags[1] == '1') {
-                    if (added_flags == true) {
-                        user_.innerHTML = user_.innerHTML + ", " + "CO_Lang";
-                    } else {
-                        user_.innerHTML = user_.innerHTML + " | " + "CO_Lang";
-                        added_flags = true;
-                    }
+                    button.style.backgroundColor = "#FF6961";
                 }
                 if (flags[1] == '2') {
-                    if (added_flags == true) {
-                        user_.innerHTML = user_.innerHTML + ", " + "ML_Lang";
-                    } else {
-                        user_.innerHTML = user_.innerHTML + ", " + "ML_Lang";
-                        added_flags = true;
-                    }
+                    button.style.backgroundColor = "yellow";
                 }
             }
-            user_.parentElement.parentElement.appendChild(button)
+            user_.parentElement.parentElement.appendChild(button);
+
+            button.onclick = function () {
+                let index = people.indexOf(user_.innerHTML);
+                if (index != -1) {
+                    let flags = flags_list[index];
+                    let added_flags = false;
+                    let string_res = "";
+                    if (flags[0] == '1') {
+                        string_res = string_res + "This user is in our crowd-sourced database for altering photos";
+                        added_flags = true;
+                    }
+                    if (flags[0] == '2') {
+                        string_res = string_res + "This user is suspected to alter their photos";
+                        added_flags = true;
+                    }
+                    if (flags[1] == '1') {
+                        if (added_flags == true) {
+                            string_res = string_res + " and is in our database for spreading nutritional misinformation.";
+                        } else {
+                            string_res = string_res + "" + "This user is our database for spreading nutritional misinformation";
+                            added_flags = true;
+                        }
+                    }
+                    if (flags[1] == '2') {
+                        if (added_flags == true) {
+                            string_res = string_res + " and is suspected to spread nutritional misinformation";
+                        } else {
+                            string_res = string_res + "" + "This user is suspected to spread nutritional misinformation";
+                            added_flags = true;
+                        }
+                    }
+                    window.alert(string_res);
+                }
+                // let temp = document.createElement("div");
+                // temp.classList.add('modal');
+
+                // let child = document.createElement('div');
+                // child.classList.add('child');
+                // child.innerHTML = "Hello"
+
+                // temp.appendChild(child);
+                // document.body.appendChild(temp);
+            };
         }
     };
     // alert(`Stored ${all_users.length} usernames`);
