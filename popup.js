808ffc2d99ff339b1ed346cf7858ffcba31eb27d
vineelgajjala <gajjala.vineel@gmail.com>
2022-03-05 18:02:14 -0600

    popup fully done, added button that sends to options page and updated css further

--- popup.js
+++ popup.js
@@ -43,3 +43,14 @@
     }
   });
 })();
+
+
+document.querySelector('#go-to-options').addEventListener('click', function() {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL('options.html'));
+    }
+  });
+
+
